fa3b30769e42dd90e4f6d92f92e7f0ebd8645a26
Nematullah Khalid <naimt.khalid@gmail.com>
2024-07-17 18:57:32 -0400

    get student by id

--- src/main/java/com/studentDirectory/studentInfo/controller/StudentController.java
+++ src/main/java/com/studentDirectory/studentInfo/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,11 @@ public class StudentController {
     public ResponseEntity<List<Student>> getAllStudent(){
         List<Student> students=studentService.getAllStudents();
         return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable Long id){
+        Student student=studentService.getStudentById(id);
+        return new ResponseEntity<>(student,HttpStatus.OK);
     }
 
 }
--- src/main/java/com/studentDirectory/studentInfo/service/StudentImpl.java
+++ src/main/java/com/studentDirectory/studentInfo/service/StudentImpl.java
@@ -2,10 +2,13 @@ package com.studentDirectory.studentInfo.service;
 
 import com.studentDirectory.studentInfo.entity.Student;
 import com.studentDirectory.studentInfo.repository.StudentRepo;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentImpl implements StudentService{
@@ -19,5 +22,15 @@ public class StudentImpl implements StudentService{
     @Override
     public List<Student> getAllStudents() {
         return studentRepo.findAll();
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        Optional<Student> student= studentRepo.findById(id);
+        if(student.isPresent()){
+            return student.get();
+        }else {
+            throw new EntityNotFoundException("Student Not Found with Id: "+id);
+        }
     }
 }
