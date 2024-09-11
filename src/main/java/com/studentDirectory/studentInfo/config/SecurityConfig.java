f4033e820b3d954d82f1909d4a1d89af0d30de66
Nematullah Khalid <naimt.khalid@gmail.com>
2024-09-11 14:22:50 -0400

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/studentDirectory/studentInfo/config/SecurityConfig.java
+++ src/main/java/com/studentDirectory/studentInfo/config/SecurityConfig.java
@@ -7,8 +7,15 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -23,8 +30,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
                         configurer
-                                .requestMatchers(HttpMethod.GET, "/api/student/all").hasRole("EMPLOYEE")
-                                .requestMatchers(HttpMethod.GET, "/api/student/**").hasRole("EMPLOYEE")
+                                .requestMatchers(HttpMethod.GET, "/api/student/all").hasAnyRole("EMPLOYEE")
+                                .requestMatchers(HttpMethod.GET, "/api/student/**").hasAnyRole("EMPLOYEE")
                                 .requestMatchers(HttpMethod.POST, "/api/student/create").hasRole("MANAGER")
                                 .requestMatchers(HttpMethod.PUT, "/api/student/update/**").hasRole("MANAGER")
                                 .requestMatchers(HttpMethod.DELETE, "/api/student/delete/**").hasRole("ADMIN")
@@ -35,8 +42,21 @@ public class SecurityConfig {
         // Disable Cross Site Request Forgery (CSRF)
         // In general, not required for stateless Rest APIs that use GET, POST, PUT and DELETE
         http.csrf(csrf -> csrf.disable());
-
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        UrlBasedCorsConfigurationSource source = new
+                UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(""));
+        config.setAllowedMethods(List.of(""));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(false);
+        config.applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**",config);
+        return source;
     }
 
     /*
